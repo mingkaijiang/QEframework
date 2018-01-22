7ef986163e6b54de7bca1fb57d31c07af01ec09e
mingkaijiang <shanyujordan512@gmail.com>
2018-01-23 09:17:50 +1100

    R markdown first trial - continue to work on it.

---
+++ R_Markdown/notes.R
@@ -0,0 +1,5 @@
+#### Note for R markdown files
+
+### All R markdown files are stored in the folder R_Markdown
+### To run any script, you need to copy the file into the directory above, i.e. the master level
+### in order to avoid conflicts of the working directory. 
