d89f25fff83fee9187bc7ed680f1de1a374bc5f5
mingkaijiang <shanyujordan512@gmail.com>
2018-05-15 13:25:12 +1000

    updated x axis label

--- QE_Scripts/Analytical_Run9.R
+++ QE_Scripts/Analytical_Run9.R
@@ -176,8 +176,8 @@ Perform_Analytical_Run9 <- function(f.flag = 1) {
         ### shoot nc vs. NPP
         plot(out350DF$nc, out350DF$NPP_photo, xlim=c(0.001, 0.03),
               ylim=c(0.5, 2.0), 
-             type = "l", xlab = "Shoot N:C ratio", 
-             ylab = expression(paste("Production [kg C ", m^-2, " ", yr^-1, "]")),
+             type = "l", xlab = "Leaf N:C ratio", 
+             ylab = expression(paste("NPP [kg C ", m^-2, " ", yr^-1, "]")),
              col="cyan", lwd = 3, cex.lab=1.5)
 
         abline(h = seq(0.5, 3.0, 0.5), v = seq(0.01, 0.05, 0.01), col="lightgray", lty = 3)
