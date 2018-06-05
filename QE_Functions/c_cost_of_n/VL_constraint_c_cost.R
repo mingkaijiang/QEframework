7aa8f8532059bd0d4812e30b070bd6f240fe0671
mingkaijiang <shanyujordan512@gmail.com>
2018-06-05 11:21:19 +1000

    still unsure about FUN model variable CN costs

--- QE_Functions/c_cost_of_n/VL_constraint_c_cost.R
+++ QE_Functions/c_cost_of_n/VL_constraint_c_cost.R
@@ -2,10 +2,9 @@ VL_constraint_c_cost <- function(nfdf, potnpp) {
 
     ### plant C:N ratio
     rcn <- 1/(nfdf$nf * nfdf$af + nfdf$nw * nfdf$aw + nfdf$nr * nfdf$ar)
-    #rcn <- seq(300, 10, length.out = length(potnpp))
-    
+
     ### mineral N pool return in kg N m-2 
-    Nmin <- Nin / leachn
+    Nmin <- Nin # / leachn
 
     ### calculate passive N uptake kg N m-2 yr-1
     Npass <- Nmin * (et/sd)
@@ -28,15 +27,18 @@ VL_constraint_c_cost <- function(nfdf, potnpp) {
     ### Calculate C spent on uptake N
     Cacq <- (rcn - potnpp / Npass2) / (1 / cost - 1 / Npass2)
     
+    ### Calculate real C spent on uptake
+    ### incorporating variable CN cost
+    modifier <- dynamic_CN_modifier(rcn, cost)
+    Cacq_real <- Cacq * modifier
+    
     ## NPP allocated to growth
-    Cgrow <- potnpp - Cacq
-  
+    Cgrow <- potnpp - Cacq_real
+  points(nfseq, Cgrow, col="green")
     ### out df
-    out <- data.frame(Cgrow, Cacq, potnpp, cost)
+    out <- data.frame(Cgrow, Cacq_real, potnpp, cost)
     colnames(out) <- c("NPP_grow", "NPP_acq", "NPP_pot", "cost")
     
     return(out)   
 }
 
-### a summary of issues:
-### 3. Variable plant N:C ratio not considered. Need to incorporate this. 
--- QE_Functions/c_cost_of_n/dynamic_CN_modifier.R
+++ QE_Functions/c_cost_of_n/dynamic_CN_modifier.R
@@ -1,19 +1,32 @@
-dynamic_CN_modifier <- function(cn_plant) {
+dynamic_CN_modifier <- function(cn_plant, N_cost) {
     ### Calculate a modifier for dynamic CN ratios
     
     ### Define constants
     ### Parameters fitted to give flexible CN ranges over the perating range of N costs of the model
     acn_flex <- 0.0 # operates as the Ncost above which there is a modification in the C expenditure (to allow higher CN ratio)
-    bcn_flex <- 0.0 # scalar that determines how much the C expenditure is modified for a given discrepancy between acn and the actual cost of uptake
+    bcn_flex <- 1 # scalar that determines how much the C expenditure is modified for a given discrepancy between acn and the actual cost of uptake
+    ccn_flex <- 150.0
     target_cn <- 20
+    
+    ### modifier - response of C expenditure to N uptake cost
+    mod <- pmax(0.0, 1.0 - (N_cost - acn_flex) / bcn_flex)
     
     ### CN ratio of the plant
     delta_cn <- cn_plant - target_cn
     
+    mod2 <- c()
     
-    ### modifier
-    mod <- pmax(0.0, 1.0 - (N_cost - acn_flex) / bcn_flux)
+    ### modifier - response of C expenditure to plant C:N ratios
+    for (k in 1:length(delta_cn)) {
+        if (delta_cn[k] > 0) {
+            mod2[k] <- mod[k] + 0.5 * (delta_cn[k]/ccn_flex)
+        } else if (delta_cn[k] < 0) {
+            mod2[k] <- mod[k] + (1 - mod[k]) * pmin(1, delta_cn[k]/ccn_flex)
+        }
+    }
+
+    ### prevent unrealistically high CN ratios
+    mod3 <- pmax(pmin(1.0, mod2), 0.5)
     
-    
-    return(mod)
+    return(mod3)
 }
