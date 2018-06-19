6e89d0b425d5dc4f160dc3d664ec24ff6ea19c6d
mingkaijiang <shanyujordan512@gmail.com>
2018-06-19 12:38:52 +1000

    investigate only exudation effect

---
+++ QE_Functions/priming/M_constraint_no_prim.R
@@ -0,0 +1,44 @@
+
+### Function for nutrient N constraint in medium term 
+# considering priming effect on slow SOM
+M_constraint_no_prim <- function(df, a, C_pass, C_slow, Nin_L) {
+    # passed are df and a, the allocation and plant N:C ratios
+    # parameters : 
+    # Nin is fixed N inputs (N deposition annd fixation) in g m-2 yr-1 (could vary fixation)
+    # nleach is the rate of n leaching of the mineral pool (per year)
+    # Tsoil is effective soil temperature for decomposition
+    # Texture is the fine soil fraction
+    # ligfl and ligrl are the lignin:C fractions in the foliage and root litter
+    # Cpass is the passive pool size in g C m-2
+    # ncp is the NC ratio of the passive pool in g N g-1 C
+    
+    # set up stuffs
+    len <- length(df)
+    ans <- c()
+    
+    # passive pool burial 
+    s_coef <- soil_coef(df, a)
+    omega_ap <- a$af*s_coef$omega_af_pass + (a$ar-a$ar*a$ariz)*s_coef$omega_ar_pass 
+    omega_as <- a$af*s_coef$omega_af_slow + (a$ar-a$ar*a$ariz)*s_coef$omega_ar_slow 
+    
+    # N comes from decomposition of pass and slow pools
+    Npass <- (1-s_coef$qq_pass) * s_coef$decomp_pass * C_pass * ncp
+    Nslow <- (1-s_coef$qq_slow) * s_coef$decomp_slow * C_slow * ncs
+    
+    # equation for N constraint with passive, wood, and leaching
+    U0 <- Nin_L + Npass + Nslow   
+    nwood <- a$aw*a$nw
+    nburial <- omega_ap*ncp + omega_as*ncs
+    nleach <- leachn/(1-leachn) * (a$nfl*a$af + a$nr*(a$ar) + a$nw*a$aw)
+    
+    ### return g C m-2 yr-1
+    NPP_NC <- U0 / (nwood + nburial + nleach)   
+    
+    ### return kg C m-2 yr-1
+    NPP <- NPP_NC*10^-3 
+    
+    df <- data.frame(NPP, Npass, Nslow, Nin_L-Nin, nwood, nburial, nleach)
+    colnames(df) <- c("NPP", "npass", "nslow", "nwoodrel", "nwood", "nburial", "nleach")
+    
+    return(df)   
+}
---
+++ QE_Functions/priming/solve_M_no_prim.R
@@ -0,0 +1,18 @@
+# Find the medium term equilibrium nf and NPP under standard conditions - by finding the root
+# for priming
+solve_M_no_prim <- function(CO2,C_pass,C_slow,Nin_L) {
+    fn <- function(nf) {
+        photo_constraint_full(nf, alloc_prim(nf),
+                              CO2) - M_constraint_no_prim(nf,alloc_prim(nf), C_pass, C_slow, Nin_L)$NPP
+        
+    }
+    equilnf <- uniroot(fn,interval=c(0.001,0.1))$root
+    
+    equilNPP <- photo_constraint_full(equilnf, 
+                                      alloc_prim(equilnf), CO2)
+
+    ans <- data.frame(equilnf,equilNPP)
+    colnames(ans) <- c("nf", "NPP")
+    
+    return(ans)
+}
